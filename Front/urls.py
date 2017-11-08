80b54cd2ae78e20f5106a941562ec1661776ea7b
m.srouji <sokomo873@free.fr>
2017-11-08 08:13:43 +0100

    Start add of prices

--- Front/urls.py
+++ Front/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^about$', about),
     url(r'^404', not_found),
+    url(r'^register', register),
+    url(r'^prices', prices),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- Front/views.py
+++ Front/views.py
@@ -13,3 +13,11 @@ def about(request):
 
 def not_found(request):
     return render(request, 'client/404.html')
+
+
+def register(request):
+    return render(request, 'client/login-register.html')
+
+
+def prices(request):
+    return render(request, 'client/prices.html')
